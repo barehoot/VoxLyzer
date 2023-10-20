d2040428bd916c15964712211882df78ee6635fa
barehoot <102745394+barehoot@users.noreply.github.com>
2023-10-20 05:32:51 +0530

    Create text_sentiment.py

---
+++ text_sentiment.py
@@ -0,0 +1,7 @@
+# text_sentiment.py
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+def text_sent(content):
+    sia = SentimentIntensityAnalyzer()
+    # Your sentiment analysis code here
