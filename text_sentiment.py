43566b1c43171f2bdef61e585c7d7cf94833c9b2
barehoot <102745394+barehoot@users.noreply.github.com>
2023-10-28 18:32:51 +0530

    Delete text_sentiment.py

--- text_sentiment.py
+++
@@ -1,8 +0,0 @@
-# text_sentiment.py
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-def text_sent(content):
-    sia = SentimentIntensityAnalyzer()
-    # Your sentiment analysis code here
