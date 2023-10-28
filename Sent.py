c37bc9e5751992814371245ee71158e4c7e5664c
barehoot <102745394+barehoot@users.noreply.github.com>
2023-10-28 19:55:08 +0530

    Update Sent.py

--- Sent.py
+++ Sent.py
@@ -28,7 +28,7 @@ nltk.download('words')
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
-
+nltk.download('averaged_perceptron_tagger')
 
 warnings.filterwarnings('ignore')
 
